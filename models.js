8902edcaadf4401b36771b3f4f1991e7941bd003
Anton Potgieter <anton@twk.co.za>
2017-09-18 13:10:09 +0200

    completed

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/anton-reg";
+const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost:27017/anton-reg";
 
 module.exports = function(mongoUrl){
   mongoose.connect(mongoUrl);
